e9f23fa9fa5e379c5f7a6a82f23dc24aa3f785b5
Arda Talu <ardatalu.98@gmail.com>
2020-11-18 21:16:32 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,22 +4,37 @@
 
 int main()
 {
-  int dizi[10];
-  int i, j, gecici, sayac;
-  printf("10 tane sayi giriniz:\n");
-  for(int i=0; i<10; i++){
-    scanf("%d", &dizi[i]);
-  }
-  for(i=0; i<10; i++){
-        sayac = 1;
-        for(j=i+1; j<10; j++){
-            if(dizi[i] == dizi[j]){
-                sayac++;
+    int dizi[10];
+    int sayac[10];
+    int i, j, gecici;
+    printf("10 tane sayi giriniz:\n");
+    for(int i=0; i<10; i++)
+    {
+        scanf("%d", &dizi[i]);
+    }
+    for(i=0; i<10; i++)
+    {
+        sayac[i] = 1;
+        for(j=i+1; j<10; j++)
+        {
+            if(dizi[i] == dizi[j])
+            {
+                sayac[i]=sayac[i]+1;
                 dizi[j] = -1;
+                if(sayac[i] > sayac[j])
+                {
+                    gecici = sayac[i];
+                    sayac[i] = sayac[j];
+                    sayac[j] = gecici;
+                }
             }
         }
-  }
-            printf("%d -> %d adet\n", dizi[i], sayac);
+        if(dizi[i] != -1)
+        {
+            printf("%d -> %d adet\n", dizi[i], sayac[i]);
+        }
+    }
 
-  return 0;
+
+    return 0;
 }
